9e3ae5a50bfa36257841b52baeeac4622500fd9b
Claudio Walser <claudio.walser@srf.ch>
2018-08-23 01:48:31 -0700

    emergency commit before setup

---
+++ docker_manager/plugins/abstract.py
@@ -0,0 +1,14 @@
+
+class BasePlugin(object):
+
+  container = None
+  name = None
+  settings = None
+
+  #def __init__(self, container):
+  def __init__(self):
+    pass
+    #self.container = container
+    #self.name = self.container.getName()
+    #self.settings = self.container.getSettings()
+
