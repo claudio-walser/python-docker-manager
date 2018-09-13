d6492faa5a36ae8e478cbe025d8100a479ff1eda
Claudio Walser <claudio.walser@srf.ch>
2018-09-13 08:12:24 +0200

    made setup.py a bit better

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@
 
 from setuptools import setup, find_packages
 
+def read(fpath):
+    with open(fpath, 'r') as f:
+        return f.read()
+
+def version(fpath):
+    return read(fpath).strip()
+
 setup(
-    name='docker-compose-extension',
-    version='0.0.1',
+    name='docker-manager',
+    version=version(),
     description='Extending docker-compose with tools for networking and nginx proxy config.',
+    long_description=read('README.rst'),
     author='Claudio Walser',
     author_email='claudio.walser@srf.ch',
     url='https://github.com/claudio-walser/python-docker-compose',
